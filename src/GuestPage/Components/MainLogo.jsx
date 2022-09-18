70891500c26ea1f076dda4668376944c422f0bc1
NaashNix <thilakshanaravindu@gmail.com>
2022-09-19 01:08:14 +0530

    First Commit

---
+++ src/GuestPage/Components/MainLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLogo = () => {
+   return (
+      <div>
+         <img src="../assets/imgs/menu-bar-logo.png" alt="" />
+      </div>
+   );
+}
+
+export default MainLogo;
---
+++ src/GuestPage/GuestPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLogo from './Components/MainLogo';
+
+const GuestPage = (props) => {
+   return (
+      <div>
+         <MainLogo />
+      </div>
+   );
+}
+
+export default GuestPage;
