9f981cbc72f29c5d0c9ab1c445a6563aaa4f506f
NaashNix <thilakshanaravindu@gmail.com>
2022-09-23 10:38:55 +0530

    Router introduced

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import './App.css';
 import GuestPage from './GuestPage/GuestPage';
 import LoginPage from './LoginPage/LoginPage';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
 
-  const [loginPageLoad, setLoginPageLoad] = useState(false);
-
-  const loginButtonClickHandler = () => {
-      setLoginPageLoad(true);
-  };
-
   return (
-    <div>
-      {!loginPageLoad && <GuestPage onClick={loginButtonClickHandler} />}
-      {loginPageLoad && <LoginPage/> }
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route exact path="/" >
+              <GuestPage />
+          </Route>
+          <Route path="/login" >
+              <LoginPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/GuestPage/GuestPage.jsx
+++ src/GuestPage/GuestPage.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Article from './Components/BasicArticle/Article';
 import Header from './Components/Header/Header';
 import MenuBar from './Components/MenuBar';
+import {useHistory } from 'react-router-dom';
 
 const GuestPage = (props) => {
 
+	const history = useHistory();
+	const routeChange = () =>{
+		let path = 	'/login'
+		history.push(path);
+	}
+
    return (
 		<div>
-			<MenuBar onClick={props.onClick} />
+
+			<MenuBar onClick={routeChange} />
 			<Header />
 			<Article />
 		</div>
