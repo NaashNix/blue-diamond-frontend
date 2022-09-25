e6edd7ef39d1f9ddece01b0d7a0d3e257bcb5045
NaashNix <thilakshanaravindu@gmail.com>
2022-09-25 12:17:23 +0530

    Footer structure done

--- src/GuestPage/Components/Footer/Footer.jsx
+++ src/GuestPage/Components/Footer/Footer.jsx
@@ -10,13 +10,21 @@ const Footer = () => {
 			<div className={classes.floatingContent}>
 				<div className={classes.childArticle}>
 					<MainLogo className={classes.logo} />
-					<span className={} >Blue Diamond Hotel</span>
-					<span>Blue Diamond Hotel</span>
+					<span className={classes.addressLine}>Blue Diamond Hotel</span>
+					<span className={classes.addressLine}>No 15,</span>
+					<span className={classes.addressLine}>De Mel Road,</span>
+					<span className={classes.addressLine}>Colombo</span>
+					<span className={classes.addressLine}>Western Province</span>
+					<span className={classes.addressLine}>Sri Lanka.</span>
 				</div>
 
-            <div className={classes.childArticle} >
-               <span>Hello There!</span>
-            </div>
+				<div className={classes.childArticle}>
+					<span>This is sample footer</span>
+				</div>
+
+				<div className={classes.childArticle}>
+					<span>This is sample footer</span>
+				</div>
 
 			</div>
 			<FooterImage />
