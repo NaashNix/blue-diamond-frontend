7df06f95652ebd880d557bed32e4cc39ed30e1da
NaashNix <thilakshanaravindu@gmail.com>
2022-09-26 23:00:15 +0530

    Some files updated

--- src/SignupPage/Signup.jsx
+++ src/SignupPage/Signup.jsx
@@ -11,8 +11,36 @@ const Signup = () => {
 
 	const childToParent = (childData) => {
 			console.log(childData);
+			var date = new Date();
+			var today = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate();
+
+			childData = {
+				...childData,
+				joinedDate : today,
+				username : 'naashnix'
+			}
+
+			var formBody = [];
+			for (var property in childData) {
+				var encodedKey = encodeURIComponent(property);
+				var encodedValue = encodeURIComponent(childData[property]);
+				formBody.push(encodedKey + "=" + encodedValue);
+			}
+
+			formBody = formBody.join("&");
+
+			fetch("http://localhost:8080/app/api/customer", {
+				method: "POST",
+				headers: {
+					"Content-Type":
+						"application/x-www-form-urlencoded;charset=UTF-8",
+				},
+				body: formBody,
+			});
+
 	}
 
+	
 
 
 	return (
