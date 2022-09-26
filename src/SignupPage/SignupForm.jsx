076e755866aa5731af253cd02923ce7ea97e8074
NaashNix <thilakshanaravindu@gmail.com>
2022-09-26 17:14:02 +0530

    Some files updated

--- src/SignupPage/SignupForm.jsx
+++ src/SignupPage/SignupForm.jsx
@@ -5,13 +5,50 @@ import backgroundImage from '/media/naashnix/Projects/IJSE/Advanced API/Spring B
 
 const SignupForm = () => {
    return (
-      <div className={classes.parent} >
-         <img src={backgroundImage} alt="" className={classes.backgroundImage} />
-         <div className={classes.form} >
-            <SignupInput type="text" />
-         </div>
-      </div>
-   );
+		<div className={classes.parent}>
+			<img
+				src={backgroundImage}
+				alt=""
+				className={classes.backgroundImage}
+			/>
+			<form className={classes.form}>
+				<SignupInput
+					type="text"
+					placeholder={"John"}
+					id={"firstName"}
+					label={"First Name"}
+               isValid={true}
+				/>
+
+				<SignupInput
+					type="text"
+					placeholder={"Wick"}
+					id={"secondName"}
+					label={"Given Name"}
+               isValid={false}
+				/>
+
+				<SignupInput
+					type="email"
+					placeholder={"sample@sample.com"}
+					id={"email"}
+					label={"E-mail Address"}
+				/>
+
+				<SignupInput
+					type="text"
+					placeholder={"200129100580"}
+					id={"nicNumber"}
+					label={"NIC Number"}
+				/>
+
+            <a className={classes.nextButton} href="">
+               Next
+            </a>
+
+			</form>
+		</div>
+	);
 }
 
 export default SignupForm;
--- src/SignupPage/SignupInput.jsx
+++ src/SignupPage/SignupInput.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import classes from "./SignupInput.module.css";
+import tick from '/media/naashnix/Projects/IJSE/Advanced API/Spring Boot/Blue Diamond Web/blue-diamond/src/assets/icons/tick.png';
+import cross from '/media/naashnix/Projects/IJSE/Advanced API/Spring Boot/Blue Diamond Web/blue-diamond/src/assets/icons/cross.png';
 
 const SignupInput = (props) => {
+
+
+
 	return (
-		<div>
+		<div className={classes.parent} >
+      <label className={classes.label} htmlFor={props.id}>{props.label} </label>
 			<input 
+            className={classes.input}
             type={props.type}
-            
+            id={props.id}
+            value={props.value}
+            onChange={props.onChange}
+            placeholder={props.placeholder}
           />
+      <div className={classes.verifyingBadge} >
+        { props.isValid && <img className={classes.verifyingBadgeIcon} src={tick} alt="Tick" /> }
+        { !props.isValid && <img className={classes.verifyingBadgeIcon} src={cross} alt="Cross" /> }
+      </div>
 		</div>
 	);
 };
